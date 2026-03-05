94480f9ab0355996399610144f804b99f08232b0
NaveenTm08 <sobhanaMuraleedharan08@gmail.com>
2026-03-05 13:53:23 +0530

    modules

---
+++ Build in Modules.py
@@ -0,0 +1,85 @@
+#MATH
+# --------
+import math 
+
+# a = math.factorial(6)
+# print(a)
+
+# output
+# 720
+
+# a = math.sqrt(16)
+# print(a)
+
+#output
+# 4.0
+
+
+# a = math.cos(45)
+# print(a)
+
+# output
+# 0
+
+
+# RANDOM
+# -----------
+import random
+
+# print(random.random())
+
+# print(random.randint(100,1000))
+
+# print(random.randrange(100,200,2))
+
+# a = ["apple","orange","papaya","lemon","banana"]
+# print(random.choice(a))
+
+# output
+import datetime
+# a = datetime.datetime.now()
+# print(a)
+
+# output
+# 2026-03-05 12:49:33.607530
+
+# a = datetime.datetime.today()
+# print(a)
+
+# a = datetime.date.today()
+# print(a)
+
+# output
+# 2026-03-05
+
+# a = datetime.date.today()
+# b = datetime.timedelta(days=10)
+
+# print(a+b)
+
+# output
+
+# 2026-03-15
+
+# a = datetime.date(2026,1,1)
+# b =datetime.date.today()
+# c=b-a
+# print(c.days)
+
+#output
+#63
+
+# a = datetime.datetime.today()
+# print(a.strftime("%H : %M : %S"))
+
+# output
+# 13 : 18
+
+
+# b =datetime.datetime.today()
+# print(b.strftime("Date = %d/%m/%Y\nTime = %H : %M"))
+
+# output
+# Date = 05/03/2026
+# Time = 13 : 40
+
